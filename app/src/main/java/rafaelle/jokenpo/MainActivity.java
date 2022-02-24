ce82450807ee909a80860a6de113dea375e03575
RAFAELLE FERNANDA PEREIRA NASCIMENTO <rafaelle.fpnasciment@senacsp.edu.br>
2022-02-24 09:28:46 -0300

    Adiciona arquivos

---
+++ app/src/main/java/rafaelle/jokenpo/MainActivity.java
@@ -0,0 +1,53 @@
+package rafaelle.jokenpo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    private float topo;
+    private ImageView maoa;
+    private ImageView maob;
+    private TextView player11;
+    private TextView player22;
+    private TextView result;
+    private ImageView vss;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        maoa = findViewById(R.id.mao1);
+        maob = findViewById(R.id.mao2);
+        player11 = findViewById(R.id.player1);
+        player22 = findViewById(R.id.player2);
+        result = findViewById(R.id.win);
+        vss = findViewById(R.id.vs);
+
+
+        maoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast alerta = Toast.makeText(getApplicationContext(), "kkkkkkkkkkkkkkkk", Toast.LENGTH_LONG);
+                alerta.show();
+
+            }
+
+        });
+        maob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast alerta = Toast.makeText(getApplicationContext(), "kkkkkkkkkkkkkkkk", Toast.LENGTH_LONG);
+                alerta.show();
+
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/rafaelle/jokenpo/SplashActivity.java
@@ -0,0 +1,29 @@
+package rafaelle.jokenpo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
